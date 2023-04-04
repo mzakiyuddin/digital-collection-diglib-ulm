90bd692bdd01efe054d553c7d49fac8e56370827
zakiego <belummikir@gmail.com>
2023-04-04 14:15:18 +0800

    init

--- index.ts
+++ index.ts
@@ -1,8 +1,33 @@
-import { hello } from "~/utils/hello";
+import { z } from "zod";
+import { getPage, parsePage } from "~/utils/fetch";
+import * as fs from "fs";
+
+const dataSchema = z.object({
+  no: z.string(),
+  judul: z.string(),
+  pengarang: z.string(),
+  penerbit: z.string(),
+  viewLink: z.string(),
+});
 
 const main = async () => {
-  const greeting = hello("Zakiego");
-  console.log(greeting);
+  const maxPage = 257;
+
+  const bucket = [] as z.infer<typeof dataSchema>[];
+
+  for (let page = 1; page <= maxPage; page++) {
+    console.log("[+] Scraping page " + page + " of " + maxPage);
+    const html = await getPage(page);
+    const data = parsePage(html);
+
+    data.forEach((item) => {
+      dataSchema.parse(item);
+    });
+
+    bucket.push(...data);
+  }
+
+  fs.writeFileSync("data.json", JSON.stringify(bucket, null, 2));
 };
 
 main();
---
+++ utils/fetch.ts
@@ -0,0 +1,41 @@
+import cheerio from "~/lib/cheerio";
+
+export const getPage = async (page: number) => {
+  const url = `http://digilib.ulm.ac.id/archive/digital/catalogue.php?page=${page}&key=ALL`;
+  const response = await fetch(url);
+  const html = await response.text();
+  return html;
+};
+
+export const parsePage = (html: string) => {
+  const $ = cheerio.load(html);
+  const trs = $("tr");
+  const trsFiltered = trs.slice(1);
+
+  const data = trsFiltered
+    .map((i, el) => {
+      const tds = $(el).find("td");
+
+      const [no, judul, pengarang, penerbit, viewLink, downloadLink] = tds
+        .map((i, el) => {
+          if (i === 4 || i === 5) {
+            return $(el).find("a").attr("href");
+          }
+
+          return $(el).text();
+        })
+        .get();
+
+      return {
+        no,
+        judul,
+        pengarang,
+        penerbit,
+        viewLink,
+        downloadLink,
+      };
+    })
+    .get();
+
+  return data;
+};
